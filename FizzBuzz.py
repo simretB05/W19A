1d759c94107cdab3d8e01108c7482c3aa9603419
simretB05 <happysimret@gmail.com>
2023-05-26 16:00:13 -0600

    added a function that checks if numbers are divisible by 3,5 or both

---
+++ FizzBuzz.py
@@ -0,0 +1,31 @@
+
+def num_checker(myNum_list):
+    for num in myNum_list:
+        if (num%3==0 and num%5==0 ):
+         print('FizzBuzz')
+        elif(num % 3==0):
+          print('Fizz') 
+        elif(num %5==0):
+              print('Buzz')  
+        else:
+           print(None)
+myNum_list=[27,83,15,75,95,70,9,31,36,42,27,32,95,39,63]
+numberChecked=num_checker(myNum_list)
+print(numberChecked) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
