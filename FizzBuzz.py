b677413c5b1800197017c0070aab8b1849e6fd87
simretB05 <happysimret@gmail.com>
2023-05-26 16:50:30 -0600

    added a function that checks if list of numbers are divisible by 3 ,5 or both

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -8,7 +8,6 @@ def num_checker(myNum_list):
           print('Fizz') 
         elif(num %5==0):
               print('Buzz')  
-           
 myNum_list=[27,83,15,75,95,70,9,31,36,42,27,32,95,39,63]
 numberChecked=num_checker(myNum_list)
 print(numberChecked)  
