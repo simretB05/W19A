b1bbc70ca0f69db7b93dd81ab0a76d865d42c82e
simretB05 <happysimret@gmail.com>
2023-05-30 18:44:43 -0600

    updated function to take one number insted of a list of numers

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,16 +1,23 @@
+def num_checker(num):
+    if (num%3==0 and num%5==0 ):
+      print('FizzBuzz')
+    elif(num % 3==0):
+      print('Fizz') 
+    elif(num %5==0):
+          print('Buzz')  
+# numberChecked=num_checker(myNum_list)
 
-def num_checker(myNum_list):
+# print(numberChecked) 
 
-    for num in myNum_list:
-        if (num%3==0 and num%5==0 ):
-         print('FizzBuzz')
-        elif(num % 3==0):
-          print('Fizz') 
-        elif(num %5==0):
-              print('Buzz')  
 myNum_list=[27,83,15,75,95,70,9,31,36,42,27,32,95,39,63]
-numberChecked=num_checker(myNum_list)
-print(numberChecked)  
+
+for num in myNum_list:
+    num_checker(num)
+    
+
+
+
+ 
 
 
 
